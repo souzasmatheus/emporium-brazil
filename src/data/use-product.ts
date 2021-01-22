945067fb0604412d119fd1b888178fca7d3ec105
Matheus Souza <matheu.sk.9@hotmail.com>
2021-01-21 23:55:01 -0300

    Resolve product type

--- src/data/use-product.ts
+++ src/data/use-product.ts
@@ -2,10 +2,14 @@ import { useCollection } from "@nandorojo/swr-firestore";
 interface Props {
   slug: string;
 }
-export default function useProduct({ slug }: Props) {
-  const { data, mutate, error } = useCollection('products')
 
-  console.log(`slug: ${slug}`)
+type Product = {
+  slug: string;
+}
+
+export default function useProduct({ slug }: Props) {
+  const { data, mutate, error } = useCollection<Product>('products')
+
   const loading = !data && !error;
   // need to remove when you using real API integration
   let product = data?.filter((current) => current.slug === slug);
