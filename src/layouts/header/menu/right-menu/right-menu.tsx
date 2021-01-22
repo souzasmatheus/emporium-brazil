f344fb38615ce07942be20acf3aa691e166368e1
Matheus Souza <matheu.sk.9@hotmail.com>
2021-01-21 23:37:14 -0300

    Remove need help

--- src/layouts/header/menu/right-menu/right-menu.tsx
+++ src/layouts/header/menu/right-menu/right-menu.tsx
@@ -8,14 +8,6 @@ import { RightMenuBox } from './right-menu.style';
 export const RightMenu: React.FC = () => {
   return (
     <RightMenuBox>
-      <NavLink
-        className="menu-item"
-        href={HELP_MENU_ITEM.href}
-        label={HELP_MENU_ITEM.defaultMessage}
-        intlId={HELP_MENU_ITEM.id}
-        iconClass="menu-icon"
-        icon={<HelpIcon />}
-      />
       <LanguageSwitcher />
     </RightMenuBox>
   );
